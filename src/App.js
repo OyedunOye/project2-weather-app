2e2fac8a77ebc6c4e6a691b9be5598085caa539f
OyedunOye <oyesinaoyedun@yahoo.com>
2024-12-06 22:30:13 +0100

    React app initialized with tailswindcss set up

---
+++ src/App.js
@@ -0,0 +1,10 @@
+
+function App() {
+  return (
+    <div className="text-red-800 flex items-center justify-center h-screen w-full border border-green-300">
+      <h1 className="text-[30px]"> Hello world!</h1>
+    </div>
+  );
+}
+
+export default App;
