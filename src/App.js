ccbc5e88198ba0b8f109d2b031d809ba800b8e11
Odo Peter Ebere <odopeter33@gmail.com>
2024-12-09 18:48:54 +0100

    feat: implemented project UI layout

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+import ForecastContainer from "./components/ForecastContainer";
+import Sidebar from "./components/Sidebar";
 
 function App() {
   return (
-    <div className="text-red-800 flex items-center justify-center h-screen w-full border border-green-300">
-      <h1 className="text-[30px]"> Hello world!</h1>
-    </div>
+    <main className="h-screen w-full bg-slate-950 text-white flex flex-col gap-4 p-4 overflow-hidden">
+      <div className="h-full w-full flex justify-center gap-x-6">
+        <div className="w-[8%] border">
+          <Sidebar />
+        </div>
+        <div className="w-[58%] border">
+          <ForecastContainer />
+        </div>
+        <div className="w-[32%] border">Hello</div>
+      </div>
+    </main>
   );
 }
 
