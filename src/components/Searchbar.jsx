4b3a783bacaa6d96909b7a1557a1ad3f3b95d24b
Odo Peter Ebere <odopeter33@gmail.com>
2024-12-17 09:51:07 +0100

    feat: implemented the API call functionality

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -1,16 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import { FaSearch } from "react-icons/fa";
+import { getWeather } from "../services/api.service";
 
 const Searchbar = () => {
+  const [textInput, setTextInput] = useState("");
+  const [weatherData, setWeatherData] = useState([]);
+
+  const handleGetCityWeather = async (e) => {
+    e.preventDefault();
+
+    const data = await getWeather(textInput);
+    setWeatherData(data);
+  };
+
+  console.log(weatherData);
+
   return (
     <form className="w-full relative">
       <input
         type="text"
+        value={textInput}
+        onChange={(e) => setTextInput(e.target.value)}
         className="w-full py-2 px-6 bg-slate-800 rounded-[10px] border-none outline-none"
         placeholder="Search city...."
       />
 
-      <button className="absolute h-5 w-5 right-6 top-1/2 -translate-y-1/2">
+      <button
+        onClick={handleGetCityWeather}
+        className="absolute h-5 w-5 right-6 top-1/2 -translate-y-1/2"
+      >
         <FaSearch className="w-full h-full opacity-80 hover:opacity-100" />
       </button>
     </form>
--- src/services/api.service.js
+++ src/services/api.service.js
@@ -1,1 +1,24 @@
-console.log("API SERVICE FILE");
+import axios from "axios";
+
+// const API_KEY = process.env.OPEN_WEATHER_API_KEY;
+// const BASE_URL = process.env.BASE_URL;
+
+const API_KEY = "d98d9acea541fe8bff32362c1693637c";
+const BASE_URL = "https://api.openweathermap.org/data/2.5/weather/";
+
+export const getWeather = async (city) => {
+  const params = {
+    q: city,
+    appid: API_KEY,
+    units: "metric",
+  };
+
+  try {
+    const response = await axios.get(BASE_URL, { params: params });
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
+
+  // const fullUrl = `${BASE_URL}q=${city}&appid=${API_KEY}&units=metric`
+};
