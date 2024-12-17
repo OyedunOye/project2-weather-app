8d0be2bb61da5e9f702d4cf72d88695c17757215
Odo Peter Ebere <odopeter33@gmail.com>
2024-12-17 09:57:14 +0100

    fix: added API key to the env file for security

--- src/services/api.service.js
+++ src/services/api.service.js
@@ -1,10 +1,7 @@
 import axios from "axios";
 
-// const API_KEY = process.env.OPEN_WEATHER_API_KEY;
-// const BASE_URL = process.env.BASE_URL;
-
-const API_KEY = "d98d9acea541fe8bff32362c1693637c";
-const BASE_URL = "https://api.openweathermap.org/data/2.5/weather/";
+const API_KEY = process.env.REACT_APP_OPEN_WEATHER_API_KEY;
+const BASE_URL = process.env.REACT_APP_BASE_URL;
 
 export const getWeather = async (city) => {
   const params = {
