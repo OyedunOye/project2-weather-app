c2a0e1aa5367a11d7334cbc98555cd6a853facaf
Odo Peter Ebere <odopeter33@gmail.com>
2024-12-18 22:40:36 +0100

    feat: passed props and data from parent components downward

--- src/App.js
+++ src/App.js
@@ -1,6 +1,30 @@
+import { useState } from "react";
+import { getWeather } from "./services/api.service";
 import { ForecastContainer, FutureForecasts, Sidebar } from "./components";
 
 function App() {
+  const [textInput, setTextInput] = useState("");
+  const [weatherData, setWeatherData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleGetCityWeather = async (e) => {
+    e.preventDefault();
+    setIsLoading(true);
+
+    try {
+      const data = await getWeather(textInput);
+      setWeatherData(data);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  const onChangeText = (e) => {
+    setTextInput(e.target.value);
+  };
+
   return (
     <main className="h-screen w-full bg-slate-950 text-white flex flex-col gap-4 p-4 overflow-hidden">
       <div className="h-full w-full flex justify-center gap-x-6">
@@ -8,7 +32,13 @@ function App() {
           <Sidebar />
         </div>
         <div className="w-[58%]">
-          <ForecastContainer />
+          <ForecastContainer
+            textInput={textInput}
+            onChangeText={onChangeText}
+            handleGetCityWeather={(e) => handleGetCityWeather(e)}
+            weatherData={weatherData}
+            isLoading={isLoading}
+          />
         </div>
         <div className="w-[32%] rounded-xl bg-slate-800 mt-20">
           <FutureForecasts />
--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -1,35 +1,31 @@
-import React, { useState } from "react";
 import { FaSearch } from "react-icons/fa";
-import { getWeather } from "../services/api.service";
 
-const Searchbar = () => {
-  const [textInput, setTextInput] = useState("");
-  const [weatherData, setWeatherData] = useState([]);
-
-  const handleGetCityWeather = async (e) => {
-    e.preventDefault();
-
-    const data = await getWeather(textInput);
-    setWeatherData(data);
-  };
-
-  console.log(weatherData);
-
+const Searchbar = ({
+  textInput,
+  onChangeText,
+  handleGetCityWeather,
+  isLoading,
+}) => {
   return (
     <form className="w-full relative">
       <input
         type="text"
         value={textInput}
-        onChange={(e) => setTextInput(e.target.value)}
+        onChange={(e) => onChangeText(e)}
         className="w-full py-2 px-6 bg-slate-800 rounded-[10px] border-none outline-none"
         placeholder="Search city...."
       />
 
       <button
         onClick={handleGetCityWeather}
-        className="absolute h-5 w-5 right-6 top-1/2 -translate-y-1/2"
+        disabled={isLoading}
+        className={`absolute h-5 w-5 right-6 top-1/2 -translate-y-1/2 ${
+          isLoading
+            ? "opacity-40 cursor-not-allowed"
+            : "opacity-80 hover:opacity-100"
+        }`}
       >
-        <FaSearch className="w-full h-full opacity-80 hover:opacity-100" />
+        <FaSearch className="w-full h-full" />
       </button>
     </form>
   );
