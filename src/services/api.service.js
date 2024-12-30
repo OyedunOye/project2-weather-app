bf7327984479b48f4f18ba522fa7388eb0608309
OyedunOye <oyesinaoyedun@yahoo.com>
2024-12-30 21:37:32 +0100

    update: added the api key and base url directly in this file as the environment defined variable didn't work on my end.

--- src/services/api.service.js
+++ src/services/api.service.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 
-const API_KEY = process.env.REACT_APP_OPEN_WEATHER_API_KEY;
-const BASE_URL = process.env.REACT_APP_BASE_URL;
+const API_KEY = "790e1a9e109239e9d1cadcdc5f665ed8";
+const BASE_URL = "https://api.openweathermap.org/data/2.5/weather";
 
+//d98d9acea541fe8bff32362c1693637c
 export const getWeather = async (city) => {
   const params = {
     q: city,
